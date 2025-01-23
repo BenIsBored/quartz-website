dc24bedbedd31b8972e0624af719998ee1e7f9f2
BenIsBored <xBKA@live.co.uk>
2025-01-23 16:37:21 +0000

    Testing different layout (and removing Quartz from footer)

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,10 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
+  ],
   afterBody: [],
   footer: Component.Footer({
     links: {
